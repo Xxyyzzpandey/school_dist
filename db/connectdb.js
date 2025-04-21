baba3c9c462a3b294f035948186c3d2754a7a565
ankit <ujjawalankit56023@gmail.com>
2025-04-21 19:01:07 +0530

    db added

---
+++ db/connectdb.js
@@ -0,0 +1,13 @@
+import mysql from 'mysql2';
+
+export const db = mysql
+  .createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  })
+  .promise() // Enables async/await and .execute()
